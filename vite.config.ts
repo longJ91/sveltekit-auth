7635035a303e341d77aa137960937ed66a9300ca
jeonghan ji <leo@finddy.io>
2024-01-15 14:34:58 +0900

    기본 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+// https://vitejs.dev/config/#using-environment-variables-in-config
+export default defineConfig(({mode}) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit()],
+	server: {
+		port: Number(env.SVELTEKIT_PORT)
+	}
+	}
 });
