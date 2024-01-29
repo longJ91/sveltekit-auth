6d93307a2ac07039e2efddb8ffef34b4fdbefa38
jeonghan ji <leo@finddy.io>
2024-01-29 19:56:10 +0900

    version up

--- src/lib/components/navigation/aside-navigation.svelte
+++ src/lib/components/navigation/aside-navigation.svelte
@@ -65,12 +65,12 @@
 			class:active={'promotion-card-schedule' === currentPage.split('/')[1]}
 			><span class="pr-2"><Calendar /></span>Promotion Card</a
 		>
-		<a
+		<!-- <a
 			class="inline-flex items-center rounded-md text-sm font-medium text-muted-foreground whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 relative justify-start hover:bg-transparent"
 			href="/sample-table"
 			class:active={'sample-table' === currentPage.split('/')[1]}
 			><span class="pr-2"><Combine /></span>Migration</a
-		>
+		> -->
 	</nav>
 </aside>
 
