26b43a50f06f26f9c15934416ffda6823deb439e
jeonghan ji <leo@finddy.io>
2024-01-30 20:36:35 +0900

    아이템 테이블 마이그레이션

--- src/routes/(protected)/item/+page.server.ts
+++ src/routes/(protected)/item/+page.server.ts
@@ -1,6 +1,6 @@
-import { clubURL, getHeaders } from '$lib/utils/request-util';
 import type { PageServerLoad } from './$types';
 import type { Item } from './[slug]/+page.server';
+import { clubURL, getHeaders } from '$lib/utils/request-util';
 
 type ItemInfoResponse = {
 	items: Array<Item>;
@@ -12,15 +12,14 @@ type ItemInfoResponse = {
 
 export const load: PageServerLoad = async ({ url }: any) => {
 	const page: string | undefined = url.searchParams.get('page');
-	const windowSize: string | undefined = url.searchParams.get('windowSize');
-	const status: string | undefined = url.searchParams.get('status');
+	// const windowSize: string | undefined = url.searchParams.get('windowSize');
+	const windowSize: number = 9999;
 	const createDate: string | undefined = url.searchParams.get('createDate');
 	let queryParams: string = '?';
 	if (page) queryParams = queryParams.concat('page=' + page + '&');
 	if (windowSize) queryParams = queryParams.concat('windowSize=' + windowSize + '&');
-	if (status) queryParams = queryParams.concat('status=' + status + '&');
 	if (createDate) queryParams = queryParams.concat('createDate=' + createDate + '&');
-
+	
 	const res = await fetch(clubURL+'/v1/admin/items' + queryParams, {
 		method: 'GET',
 		headers: getHeaders()
