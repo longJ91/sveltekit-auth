82d4a4bebca220a6b31c8e6231d00fcaea87208e
jeonghan ji <leo@finddy.io>
2024-01-30 17:22:39 +0900

    클럽 테이블 생성 버튼 삭제

--- src/routes/(protected)/club/+page.svelte
+++ src/routes/(protected)/club/+page.svelte
@@ -124,14 +124,6 @@
 			<p>Club</p>
 		</div>
 		<div class="flex items-center py-4">
-			<Button
-				class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-				variant="secondary"
-				on:click={() =>
-					goto('/club/create', {
-						invalidateAll: true
-					})}>Create</Button
-			>
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger asChild let:builder>
 					<Button variant="outline" class="ml-auto" builders={[builder]}>
