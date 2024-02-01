21b343829e379f7ddacd0efbe42dfb30d53f508d
jeonghan ji <leo@finddy.io>
2024-02-01 16:38:29 +0900

    생성 / 업데이트 페이지 action 버튼 위치 조정

--- src/lib/components/custom-form/club-event-form.svelte
+++ src/lib/components/custom-form/club-event-form.svelte
@@ -118,20 +118,21 @@
 		<p class="text-sm text-muted-foreground">This is country.</p>
 	</div>
 
-	{#if button == 'Create'}
-		<Form.Button
-			class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-			>{button}</Form.Button
+	<div class="flex justify-end mt-6">
+		<Button
+			class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+			on:click={() => goto('/club-event', { replaceState: true })}>Cancel</Button
 		>
-	{:else}
-		<Form.Button
-			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-			>{button}</Form.Button
-		>
-	{/if}
-
-	<Button
-		class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-		on:click={() => goto('/club-event', { replaceState: true })}>Cancel</Button
-	>
+		{#if button == 'Create'}
+			<Form.Button
+				class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+				>{button}</Form.Button
+			>
+		{:else}
+			<Form.Button
+				class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+				>{button}</Form.Button
+			>
+		{/if}
+	</div>
 </Form.Root>
